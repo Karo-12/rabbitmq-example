566b7272eb1bb1476f7cefa98372922053628d87
Karolina <200626@student.pwr.edu.pl>
2022-11-16 12:38:14 +0100

    Update project

--- src/main/java/Receiver.java
+++ src/main/java/Receiver.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Receiver {
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Receiver {
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPassword("guest");
+        factory.setUsername("guest");
+
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false , false, false, null);
+        System.out.println("waiting for messages");
+
+        DeliverCallback deliverCallback = (consumerRag, delivery) -> {
+            String message = new String(delivery.getBody(), "UTF-8");
+            System.out.println("Received message: " + message +"");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {});
+    }
 }
--- src/main/java/Sender.java
+++ src/main/java/Sender.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Sender {
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+    private static final String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        factory.setPassword("guest");
+        factory.setUsername("guest");
+        try(Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            String message = "Hello World";
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println("Message sent: " + message);
+        }
+    }
 }
